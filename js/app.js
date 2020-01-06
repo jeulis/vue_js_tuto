a040848b5398739087d835a885906af621de9813
NekMerkat <jeulis8@gmail.com>
2020-01-06 16:31:53 +0100

    update css + js + html (big step up)
    v1.2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
 var vm = new Vue({
     el: '#app',
     data: {
-        todoList: ['Finir cette liste 1', 'Finir cette liste 2', 'Finir cette liste 3'],
-        doneList: ['HEHEHE', 'fait'],
+        todoList: [],
+        doingList: [],
+        doneList: [],
         element: null,
         listNumber: 0,
-        number: 9,
+        allListsLength: null,
+        doneLengthValue: null,
+        currentlyDoing: null,
+        alert: '',
+        alert_confirm: false
 
 
     },
     methods: {
-        initiateProgressBar() {
 
-        },
         pushList() {
             if (this.element !== '' & this.element !== null & this.element !== ' ') {
                 this.todoList.push(this.element);
@@ -21,33 +24,76 @@ var vm = new Vue({
                 console.log('item vide !')
             }
             this.element = '';
-            this.updateLength()
+            this.getAllLength()
         },
-        move(index, todo) {
-            this.doneList.push(todo);
-            this.todoList.splice(index, 1);
-            // this.$delete(this.todoList, index)
-
-            this.updateLength();
-
-        },
-        remove(index, done) {
+        moveToTodo(index, done) {
             this.todoList.push(done);
             this.doneList.splice(index, 1)
             // this.$delete(this.todoList, index)
 
-            this.updateLength()
+            this.getAllLength()
         },
-        clearList() {
-            this.todoList = [];
-            this.updateLength()
+        moveToDoing(index, todo) {
+            this.doingList.push(todo);
+            this.todoList.splice(index, 1);
+            // this.$delete(this.todoList, index)
 
+            this.getAllLength();
 
         },
-        updateLength() {
-            document.getElementById("progressTodo").value = this.todoList.length;
+        moveToDone(index, doing) {
+            this.doneList.push(doing);
+            this.doingList.splice(index, 1);
+            // this.$delete(this.todoList, index)
+
+            this.getAllLength();
+
+        },
+        clearList(listName) {
+            if (listName === 'todo') {
+                this.todoList = [];
+            }
+            if (listName === 'doing') {
+                this.doingList = [];
+            }
+            if (listName === 'done') {
+                this.doneList = [];
+            }
+            if (listName === 'all' && this.alert_confirm === true) {
+                this.doneList = [];
+                this.todoList = [];
+                this.doingList = [];
+            }
+            this.getAllLength()
+
+        },
+        donePercentage() {
+            this.doneLengthValue = (this.doneList.length * 100) / this.allListsLength;
+            this.doneLengthValue = Math.round(this.doneLengthValue);
+            if (this.doingList == 0 & this.todoList == 0 & this.doneList != null) {
+                this.doneLengthValue = 100;
+            }
+            document.getElementById("progressTodo").value = this.doneLengthValue;
+        },
+        doingPercentage() {
+            this.currentlyDoing = (this.doingList.length * 100) / this.allListsLength;
+            this.currentlyDoing = Math.round(this.currentlyDoing);
+
+            document.getElementById("progressTodo").value = this.doneLengthValue;
+        },
+        getAllLength() {
+            this.allListsLength = this.todoList.length + this.doingList.length + this.doneList.length;
+            this.donePercentage();
+            this.doingPercentage();
+            return this.allListsLength;
+        },
+        resetErrors() {
+            this.alert = '';
         },
 
+    },
+    mounted() {
+        this.getAllLength();
 
     }
 
